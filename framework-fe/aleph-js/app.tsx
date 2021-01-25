9812b5106be29c62408d8aedb72a14f12f0e7eea
hylerrix <hylerrix@gmail.com>
2021-01-25 19:31:20 +0800

    init nextjs ts server app
    Signed-off-by: hylerrix <hylerrix@gmail.com>

--- framework-fe/aleph-js/app.tsx
+++ framework-fe/aleph-js/app.tsx
@@ -5,6 +5,7 @@ export default function App({ Page, pageProps }: { Page: ComponentType<any>, pag
     return (
         <>
             <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>Hello World - Aleph.js</title>
             </Head>
             <Page {...pageProps} />
